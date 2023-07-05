0f932cd9305da5ca944a9271e395c5cb2a28791e
boody-cpu <81966035+boody-cpu@users.noreply.github.com>
2023-07-05 18:10:47 +0300

    update , delete store functions and ui

---
+++ src/components/Settings-form.tsx
@@ -0,0 +1,132 @@
+"use client";
+import React, { useState } from "react";
+import axios from "axios";
+import { Trash } from "lucide-react";
+import { toast } from "react-hot-toast";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { Loader2 } from "lucide-react";
+import * as z from "zod";
+import { useParams, useRouter } from "next/navigation";
+
+import { Store } from "@prisma/client";
+import Heading from "@/components/ui/heading";
+import { Button } from "@/components/ui/button";
+import { Separator } from "@/components/ui/separator";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import AlertModal from "./modals/alert-modal";
+
+interface SettingsFormProps {
+  initialData: Store;
+}
+
+const formSchema = z.object({
+  name: z.string().min(1),
+});
+
+type settingsFormValues = z.infer<typeof formSchema>;
+
+const SettingsForm: React.FC<SettingsFormProps> = ({ initialData }) => {
+  const params = useParams();
+  const router = useRouter();
+  const [open, setOpen] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
+
+  const form = useForm<settingsFormValues>({
+    resolver: zodResolver(formSchema),
+    defaultValues: initialData,
+  });
+
+  const onSubmit = async (data: settingsFormValues) => {
+    try {
+      setIsLoading(true);
+      await axios.patch(`/api/stores/${params.storeId}`, data);
+      router.refresh();
+      toast.success("Store updated.");
+    } catch (error) {
+      toast.error("Somthing went Wrong! , please try agian");
+    } finally {
+      setIsLoading(false);
+    }
+  };
+  const onDelete = async () => {
+    try {
+      setIsLoading(true);
+      await axios.delete(`/api/stores/${params.storeId}`);
+      router.refresh();
+      router.push("/");
+      toast.success("Store Deleted.");
+    } catch (error) {
+      toast.error("Make sure you removed all products and categories first.");
+    } finally {
+      setIsLoading(false);
+      setOpen(false);
+    }
+  };
+  return (
+    <>
+      <AlertModal
+        isOpen={open}
+        onClose={() => {
+          setOpen(false);
+        }}
+        loading={isLoading}
+        onConfirm={onDelete}
+      />
+      <div className="flex items-center justify-between">
+        <Heading title="Settings" description="manage Your Store" />
+        <Button
+          disabled={isLoading}
+          variant={"destructive"}
+          size={"icon"}
+          onClick={() => {
+            setOpen(true);
+          }}
+        >
+          <Trash className=" h-4 w-4" />
+        </Button>
+      </div>
+      <Separator />
+      <Form {...form}>
+        <form
+          onSubmit={form.handleSubmit(onSubmit)}
+          className="space-y-8 w-full"
+        >
+          <div className="grid grid-cols-3 gap-8">
+            <FormField
+              control={form.control}
+              name="name"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Name</FormLabel>
+                  <FormControl>
+                    <Input
+                      disabled={isLoading}
+                      placeholder="Store Name.."
+                      {...field}
+                    />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            ></FormField>
+          </div>
+          <Button disabled={isLoading} type="submit" className="ml-auto">
+            {isLoading ? <Loader2 className="mr-2 h-4 w-4 animate-spin" /> : ""}
+            Save Changes
+          </Button>
+        </form>
+      </Form>
+    </>
+  );
+};
+
+export default SettingsForm;
